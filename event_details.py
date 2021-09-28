1c5f227489b078d6004c8b97a65932588a06c612
Michelle <scottmm374@gmail.com>
2021-09-28 10:51:35 -0400

    added venue name, venue address, editions and frequency

--- event_details.py
+++ event_details.py
@@ -6,7 +6,8 @@ import requests
 # URL = 'https://10times.com/plastics-recycling-world-exhibition'
 # URL = "https://10times.com/legion-sports-fest"
 # URL = 'https://10times.com/med-tech-innovation'
-URL = 'https://10times.com/home-based-travel-agent-forum-exhibition'
+# URL = 'https://10times.com/home-based-travel-agent-forum-exhibition'  
+URL = 'https://10times.com/pour-lamour-du'
 
 
 response = requests.get(URL)
@@ -46,47 +47,51 @@ event_single['Format'] = [repr(format_type.string)]
 # Grabs the event name
 title = top_wrapper.h1.get_text()
 event_single['Event_name'] = [title]
-# for string in top_wrapper.h1.stripped_strings:
-#     print()
-# print(title)
+
 
 # Grabs location from header
 
 location_finder = top_wrapper.findAll('div', attrs={'class': 'mb-0 fs-20'})
 # 2 divs with same class name, second one has text needed. 
+
+
 location = location_finder[1].get_text()
 event_single['Location'] = [location]
 
-# print(location)
 
 # Grabbing description of event. This will need some conditionals, since some have a link to expand text 
 description_section = soup.find('div', attrs={'id': 'content'})
 description = description_section.p.get_text()
 event_single['Description']= [description]
-# print(description.p.get_text())
 
 
-# TABLE 
+# * TABLE 
 
 table_details = soup.find('table', attrs={'class': 'table noBorder mng w-100 trBorder'})
 
 # Table First Row section
 table_row = table_details.find('tr', attrs={'id': 'hvrout1'})
 
-# Timings, this will also need conditionals to search for more timings if expand is present
-timings = table_row.find('h2').next_sibling.strip().replace('\n', '').replace(' ', '')
-event_single['Timings'] = [timings]
+"""
+Timings, this will also need conditionals to search for more timings if expand is present
+? UPDATE changed to td and stripped strings, returns everything without need for clicking link (view more)
 
-# TODO Where I left off Estimated turnout
-turn_list = []
+"""
+time_list = []
+for time in table_row.find('td').stripped_strings:
+    timings = time.strip().replace('\n', '').replace(' ', '')
+    time_list.append(timings)
+event_single['Timings'] = time_list[1:-1]
+
+
+
+# *  Estimated turnout 
+#  * <----------------------------------------------->
 turnout = table_row.next_sibling
-# visitors = turnout.findAll('a').get_text()
 my_list = []
 
 for nums in (turnout.findAll('a')):
     
-    # visitors = nums[0]
-    # exhibitors = nums[1]
     my_list.append(nums.get_text().strip())
 
 visitors = (f'{my_list[0]} Visitors')
@@ -95,14 +100,37 @@ exhibitors = (f'{my_list[1]} Exhibitors')
 # print(f'{my_list[0]} Visitors \n {my_list[1]} Exhibitors')
 event_single['Estimated_turnout'] = [visitors, exhibitors]
 
-# exibitors = turnout.find
 
 
 
-# Grabbing catagories 
+# * CATAGORIES 
 table_data_cell = table_details.find('td', attrs={'id': 'hvrout2'})
 for catagory in table_data_cell.findAll('a'):
     event_single['Categories'] = [catagory.get_text()]
-    # print(catagory.get_text())
 
-    print(event_single)
+
+#  * Venue_name
+venue_list = []
+map_location = soup.find('section', attrs={'id': 'map_dirr'})
+for venue in map_location.findAll('a'):
+    venue_list.append(venue.get_text())
+event_single['Event_venue_name'] = [venue_list[1].strip().replace('\n', '')]
+
+
+venue_address = map_location.find('p')
+addy = []
+for address in venue_address.find_all('span'):
+    addy.append(address.get_text())
+event_single['Venue_address'] = addy
+
+
+table_section_3 = soup.find('tr', attrs={'id': 'hvrout3'})
+# TODO  Need to format the list to exclude headers
+edition_freq = []
+for element in table_section_3.find('td').stripped_strings:
+    edition_freq.append(element)
+print(edition_freq)
+
+
+# print(addy)
+# print(event_single)
