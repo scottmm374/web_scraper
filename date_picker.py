49c7e9a8795bc8bda269531e178760c7d8a0d49a
Michelle <scottmm374@gmail.com>
2021-10-01 11:59:56 -0400

    pulling urlbase from txt, appending one month date range to create date range url for events per country one year previous

--- date_picker.py
+++ date_picker.py
@@ -7,43 +7,45 @@ import csv
 
 todays_date = date.today()
 
-def date_range_generator (num_s, num_e):
-    todays_date = date.today()
+
+url_list = []
+url_ranges = []
+
+
+
+def date_range_generator (num_s, num_e, url):
+    # url = url_list[0]
    
     start_date = date.today() + relativedelta(months=-num_s)
     one_month = date.today() + relativedelta(months=-num_e)
-    url = (f'?datefrom={start_date}&dateto={one_month}')
+    url_ranges.append(f'{url}?datefrom={start_date}&dateto={one_month}')
     
-    print(url)
+    
+    
+def change_range():
+    for x in range(len(url_list)):
+        # if x != url_list[-1]:
+        num_start = 13
+        num_stop = 12
+        while num_stop != 0:
+            num_start = num_start - 1
+            num_stop = num_stop - 1
+            date_range_generator(num_start, num_stop, url_list[x])
 
 
+with open('countries.txt') as countries:
+    line = countries.readline()
+    while line:
+        line = countries.readline()
+        url_list.append(line.strip().replace('\n', ''))
+    change_range()
 
-# print(start_date)
-# print(one_month)
 
-# url = (f'?datefrom={start_date}&dateto={one_month}')
-# print(url)
-# print(one_month == todays_date)
+print(len(url_ranges), len(url_list)*12)
 
-
-def check_range():
-    num_start = 13
-    num_stop = 12
-    while num_stop != 0:
-        num_start = num_start - 1
-        num_stop = num_stop - 1
-        date_range_generator(num_start, num_stop)
-
-check_range()
-
-# with open('countries.txt') as countries:
-#     line = countries.readline()
-#     while line:
-#         line = countries.readline()
-#         while one_month != todays_date:
-#             url = (f'{line}?datefrom={start_date}&dateto={one_month}')
-#             num_start = num_start - 1
-#             num_stop = num_stop - 1
+        
+            
+           
         
 # "https://10times.com/usa?datefrom=2021-09-01&dateto=2022-10-07"
 
